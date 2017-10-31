d92244a8fc828c05b6b218c45bb1d8e74b566ca8
Locke <1534921818@qq.com>
2017-10-31 23:04:21 +0800

    wpf

---
+++ Float/Float/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Float
+{
+	/// <summary>
+	/// App.xaml 的交互逻辑
+	/// </summary>
+	public partial class App : Application
+	{
+		private void AppActivated(object sender, EventArgs e)
+		{
+
+		}
+
+		private void AppDeactivated(object sender, EventArgs e)
+		{
+
+		}
+
+		private void AppExit(object sender, ExitEventArgs e)
+		{
+
+		}
+
+		private void AppStartup(object sender, StartupEventArgs e)
+		{
+
+		}
+	}
+}
