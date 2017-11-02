0adf65fec71eed7af9efde684f03698b74cc7342
liucj01 <liucj01@mail.jj.cn>
2017-11-02 21:26:07 +0800

    3

--- Float/Float/MainWindow.xaml.cs
+++ Float/Float/MainWindow.xaml.cs
@@ -22,7 +22,57 @@ namespace Float
 	{
 		public MainWindow()
 		{
+			this.Activated += OnActivated;
+			this.Closing += OnClosing;
+			this.ContentRendered += OnContentRendered;
+			this.Deactivated += OnDeactivated;
+			this.Loaded += OnLoaded;
+			this.Closed += OnClosed;
+			this.Unloaded += OnUnloaded;
+			this.SourceInitialized += OnSourceInitialized;
+
 			InitializeComponent();
 		}
+
+		void OnSourceInitialized(object sender, EventArgs e)
+		{
+			Console.WriteLine("1---SourceInitialized！");
+		}
+
+		void OnUnloaded(object sender, RoutedEventArgs e)
+		{
+			Console.WriteLine("Unloaded！");
+		}
+
+		void OnClosed(object sender, EventArgs e)
+		{
+			Console.WriteLine("_Closed！");
+		}
+
+		void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			Console.WriteLine("3---Loaded！");
+		}
+
+		void OnDeactivated(object sender, EventArgs e)
+		{
+			Console.WriteLine("Deactivated！");
+		}
+
+		void OnContentRendered(object sender, EventArgs e)
+		{
+			Console.WriteLine("ContentRendered！");
+		}
+
+		void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			Console.WriteLine("---Closing！");
+		}
+
+		void OnActivated(object sender, EventArgs e)
+		{
+			Console.WriteLine("2---Activated！");
+		}
+
 	}
 }
