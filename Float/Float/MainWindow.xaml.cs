c3fbf748859f1980360930ba13aeee5f15229243
liucj01 <liucj01@mail.jj.cn>
2017-11-07 14:21:47 +0800

    style

--- Float/Float/MainWindow.xaml.cs
+++ Float/Float/MainWindow.xaml.cs
@@ -23,79 +23,36 @@ namespace Float
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		WindowState ws;
-		WindowState wsl;
+		WindowState lastState;
 		NotifyIcon notifyIcon;
 
 
 		public MainWindow()
 		{
+			this.SourceInitialized += OnSourceInitialized;
 			this.Activated += OnActivated;
-			this.Closing += OnClosing;
-			this.ContentRendered += OnContentRendered;
 			this.Deactivated += OnDeactivated;
 			this.Loaded += OnLoaded;
-			this.Closed += OnClosed;
 			this.Unloaded += OnUnloaded;
-			this.SourceInitialized += OnSourceInitialized;
+			this.Closing += OnClosing;
+			this.Closed += OnClosed;
+			this.StateChanged += OnStateChanged;
 
 			InitializeComponent();
 
-			//显示托盘。
-			icon();
-			//保证窗体显示在上方。
-			wsl = WindowState;
+			CreateNotifyIcon();
+			lastState = WindowState;
+			this.Visibility = Visibility.Hidden;
 		}
 
-		private void icon()
+		void OnActivated(object sender, EventArgs e)
 		{
-			this.notifyIcon = new NotifyIcon();
-			this.notifyIcon.BalloonTipText = "Hello, 文件监视器"; //设置程序启动时显示的文本
-			this.notifyIcon.Text = "文件监视器";//最小化到托盘时，鼠标点击时显示的文本
-			this.notifyIcon.Icon = new System.Drawing.Icon("float.ico");//程序图标
-			this.notifyIcon.Visible = true;
-			notifyIcon.MouseDoubleClick += OnNotifyIconDoubleClick;
-			this.notifyIcon.ShowBalloonTip(1000);
-
-			System.Windows.Forms.MenuItem m1 = new System.Windows.Forms.MenuItem("open");
-			m1.Click += m1_Click;
-			System.Windows.Forms.MenuItem m2 = new System.Windows.Forms.MenuItem("close");
-			m2.Click += m2_Click;
-			System.Windows.Forms.MenuItem[] m = new System.Windows.Forms.MenuItem[] { m1, m2 };
-			this.notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(m);
+			//Console.WriteLine("2---Activated！");
 		}
 
-		private void OnNotifyIconDoubleClick(object sender, EventArgs e)
+		void OnDeactivated(object sender, EventArgs e)
 		{
-			this.Show();
-			WindowState = wsl;
-		}
-
-		private void Window_StateChanged(object sender, EventArgs e)
-		{
-			ws = WindowState;
-			if (ws == WindowState.Minimized)
-			{
-				this.Hide();
-			}
-		}
-
-		void m2_Click(object sender, EventArgs e)
-		{
-			if (System.Windows.MessageBox.Show("sure to exit?",
-											   "application",
-												MessageBoxButton.YesNo,
-												MessageBoxImage.Question,
-												MessageBoxResult.No) == MessageBoxResult.Yes)
-			{
-				System.Windows.Application.Current.Shutdown();
-			}
-		}
-
-		void m1_Click(object sender, EventArgs e)
-		{
-			this.Show();
-			this.Activate();
+			//Console.WriteLine("Deactivated！");
 		}
 
 		void OnSourceInitialized(object sender, EventArgs e)
@@ -118,29 +75,20 @@ namespace Float
 			//Console.WriteLine("3---Loaded！");
 		}
 
-		void OnDeactivated(object sender, EventArgs e)
-		{
-			//Console.WriteLine("Deactivated！");
-		}
-
-		void OnContentRendered(object sender, EventArgs e)
-		{
-			//Console.WriteLine("ContentRendered！");
-		}
-
 		void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
 		{
-			//Console.WriteLine("---Closing！");
 			e.Cancel = true;
 			this.WindowState = WindowState.Minimized;
-			ws = WindowState.Minimized;
-			this.notifyIcon.Visible = true;
-			this.notifyIcon.ShowBalloonTip(30, "注意", "大家好，这是一个事例", ToolTipIcon.Info);
 		}
 
-		void OnActivated(object sender, EventArgs e)
+		private void OnStateChanged(object sender, EventArgs e)
 		{
-			//Console.WriteLine("2---Activated！");
+			if (WindowState == WindowState.Minimized)
+			{
+				this.Hide();
+				this.notifyIcon.Visible = true;
+				this.notifyIcon.ShowBalloonTip(5, "Note", "Show Main Window from here", ToolTipIcon.Info);
+			}
 		}
 
 		private void OnOKButtonClick(object sender, RoutedEventArgs e)
@@ -152,6 +100,54 @@ namespace Float
 		{
 
 		}
+
+		#region notify icon
+
+		private void CreateNotifyIcon()
+		{
+			this.notifyIcon = new NotifyIcon();
+			this.notifyIcon.BalloonTipText = "Floating fun";
+			this.notifyIcon.Text = "Floating icon";
+			this.notifyIcon.Icon = new Icon("float.ico");
+			this.notifyIcon.Visible = true;
+			notifyIcon.MouseDoubleClick += OnNotifyIconDoubleClick;
+			this.notifyIcon.ShowBalloonTip(1000);
+
+			System.Windows.Forms.MenuItem m1 = new System.Windows.Forms.MenuItem("Show Main Window");
+			m1.Click += OnClickNotifyOpenMainWindow;
+			System.Windows.Forms.MenuItem m2 = new System.Windows.Forms.MenuItem("Exit");
+			m2.Click += OnClickNotifyExit;
+			System.Windows.Forms.MenuItem[] menu = new System.Windows.Forms.MenuItem[] { m1, m2 };
+			this.notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(menu);
+		}
+
+		private void OnNotifyIconDoubleClick(object sender, EventArgs e)
+		{
+			this.Show();
+			WindowState = lastState;
+		}
+
+		void OnClickNotifyExit(object sender, EventArgs e)
+		{
+			/*if (System.Windows.MessageBox.Show("sure to exit?", "application",
+												MessageBoxButton.YesNo,
+												MessageBoxImage.Question,
+												MessageBoxResult.No) == MessageBoxResult.Yes)
+			{
+				System.Windows.Application.Current.Shutdown();
+			}*/
+			this.notifyIcon.Visible = false;
+			System.Windows.Application.Current.Shutdown();
+		}
+
+		void OnClickNotifyOpenMainWindow(object sender, EventArgs e)
+		{
+			this.Show();
+			WindowState = lastState;
+		}
+
+		#endregion
+
 	}
 
 }
