47399a183575164cc61b517eef4fef75cd080689
liucj01 <liucj01@mail.jj.cn>
2017-11-15 13:17:03 +0800

    not

--- Float/Float/MainWindow.xaml.cs
+++ Float/Float/MainWindow.xaml.cs
@@ -12,8 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Forms; // NotifyIcon control
-using System.Drawing; // Icon
 
 
 namespace Float
@@ -23,57 +21,24 @@ namespace Float
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		WindowState lastState;
-		NotifyIcon notifyIcon;
-
+		Notify notify = new Notify();
 
 		public MainWindow()
 		{
-			this.SourceInitialized += OnSourceInitialized;
 			this.Activated += OnActivated;
 			this.Deactivated += OnDeactivated;
-			this.Loaded += OnLoaded;
-			this.Unloaded += OnUnloaded;
 			this.Closing += OnClosing;
 			this.Closed += OnClosed;
 			this.StateChanged += OnStateChanged;
 
 			InitializeComponent();
 
-			CreateNotifyIcon();
-			lastState = WindowState;
+			notify.Create(this);
 			this.Visibility = Visibility.Hidden;
 		}
 
-		void OnActivated(object sender, EventArgs e)
-		{
-			//Console.WriteLine("2---Activated！");
-		}
-
-		void OnDeactivated(object sender, EventArgs e)
-		{
-			//Console.WriteLine("Deactivated！");
-		}
-
-		void OnSourceInitialized(object sender, EventArgs e)
-		{
-			//Console.WriteLine("1---SourceInitialized！");
-		}
-
-		void OnUnloaded(object sender, RoutedEventArgs e)
-		{
-			//Console.WriteLine("Unloaded！");
-		}
-
-		void OnClosed(object sender, EventArgs e)
-		{
-			//Console.WriteLine("_Closed！");
-		}
-
-		void OnLoaded(object sender, RoutedEventArgs e)
-		{
-			//Console.WriteLine("3---Loaded！");
-		}
+		void OnActivated(object sender, EventArgs e) { }
+		void OnDeactivated(object sender, EventArgs e) { }
 
 		void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
 		{
@@ -81,14 +46,28 @@ namespace Float
 			this.WindowState = WindowState.Minimized;
 		}
 
+		void OnClosed(object sender, EventArgs e)
+		{
+			notify.Destroy();
+		}
+
 		private void OnStateChanged(object sender, EventArgs e)
 		{
 			if (WindowState == WindowState.Minimized)
 			{
 				this.Hide();
-				notifyIcon.Visible = true;
-				notifyIcon.ShowBalloonTip(5, "Note", "Show Main Window from here", ToolTipIcon.Info);
+				//notifyIcon.ShowBalloonTip(3, "Note", "Show Main Window from here", ToolTipIcon.Info);
 			}
+		}
+
+		public void ShowWindow()
+		{
+			Show();
+		}
+
+		public void HideWindow()
+		{
+			Hide();
 		}
 
 		private void OnOKButtonClick(object sender, RoutedEventArgs e)
@@ -100,69 +79,6 @@ namespace Float
 		{
 
 		}
-
-		#region notify icon
-
-		private void CreateNotifyIcon()
-		{
-			notifyIcon = new NotifyIcon();
-			notifyIcon.BalloonTipText = "Floating fun";
-			notifyIcon.Text = "Floating icon";
-			notifyIcon.Icon = new Icon("float.ico");
-			notifyIcon.Visible = true;
-			notifyIcon.MouseDoubleClick += OnNotifyIconDoubleClick;
-			notifyIcon.ShowBalloonTip(1000);
-
-			System.Windows.Forms.MenuItem m1 = new System.Windows.Forms.MenuItem("Show Main Window");
-			m1.Click += OnClickNotifyOpenMainWindow;
-			System.Windows.Forms.MenuItem m2 = new System.Windows.Forms.MenuItem("Exit");
-			m2.Click += OnClickNotifyExit;
-			System.Windows.Forms.MenuItem m3 = new System.Windows.Forms.MenuItem("Send");
-			m3.Click += OnClickSend;
-			System.Windows.Forms.MenuItem[] menu = new System.Windows.Forms.MenuItem[] { m1, m2, m3 };
-			notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(menu);
-		}
-
-		private void DestroyNotifyIcon()
-		{
-			notifyIcon.Visible = false;
-			notifyIcon.Dispose();
-		}
-
-		private void OnNotifyIconDoubleClick(object sender, EventArgs e)
-		{
-			this.Show();
-			WindowState = lastState;
-		}
-
-		void OnClickNotifyExit(object sender, EventArgs e)
-		{
-			notifyIcon.Visible = false;
-			notifyIcon.Dispose();
-			App.Instance.Destory();
-		}
-
-		void OnClickNotifyOpenMainWindow(object sender, EventArgs e)
-		{
-			this.Show();
-			WindowState = lastState;
-		}
-
-		void OnClickSend(object sender, EventArgs e)
-		{
-			Command cmd = new Command();
-			cmd.cmdType = (ushort)CommandId.ShowWindow;
-			cmd.number1 = 1;
-			cmd.number2 = 22;
-			cmd.number3 = 33;
-			cmd.string1 = "s1111";
-			cmd.string2 = "s2222";
-			cmd.string3 = "s3333";
-			App.Instance.SendCommand(CommandId.ShowWindow, cmd);
-		}
-
-
-		#endregion
 
 	}
 
---
+++ Float/Float/Notify.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms; // NotifyIcon control
+using System.Drawing; // Icon
+
+
+namespace Float
+{
+	public class Notify
+	{
+		NotifyIcon notifyIcon;
+		MainWindow window;
+
+		public void Create(MainWindow window)
+		{
+			this.window = window;
+			notifyIcon = new NotifyIcon();
+			notifyIcon.BalloonTipText = "Floating fun";
+			notifyIcon.Text = "Floating";
+			notifyIcon.Icon = new Icon("float.ico");
+			notifyIcon.Visible = true;
+
+			MenuItem m1 = new MenuItem("Show Main Window");
+			m1.Click += OnOpenMainWindow;
+			MenuItem m2 = new MenuItem("Hide Main Window");
+			m2.Click += OnHideMainWindow;
+			MenuItem m3 = new MenuItem("Play This");
+			m3.Click += OnPlayThis;
+			MenuItem m6 = new MenuItem("Exit");
+			m6.Click += OnExit;
+			MenuItem m10 = new MenuItem("Send");
+			m10.Click += OnSend;
+			MenuItem[] menu = new MenuItem[] { m1, m2, m3, m6, m10 };
+			notifyIcon.ContextMenu = new ContextMenu(menu);
+		}
+
+		public void Destroy()
+		{
+			notifyIcon.Visible = false;
+			notifyIcon.Dispose();
+		}
+
+		void OnExit(object sender, EventArgs e)
+		{
+			Destroy();
+			App.Instance.Destory();
+		}
+
+		void OnOpenMainWindow(object sender, EventArgs e)
+		{
+			window.ShowWindow();
+		}
+
+		void OnHideMainWindow(object sender, EventArgs e)
+		{
+			window.HideWindow();
+		}
+
+		void OnPlayThis(object sender, EventArgs e)
+		{
+			
+		}
+
+		void OnSend(object sender, EventArgs e)
+		{
+			Command cmd = new Command();
+			cmd.cmdType = (ushort)CommandId.ShowWindow;
+			cmd.number1 = 1;
+			cmd.number2 = 22;
+			cmd.number3 = 33;
+			cmd.string1 = "s1111";
+			cmd.string2 = "s2222";
+			cmd.string3 = "s3333";
+			App.Instance.SendCommand(CommandId.ShowWindow, cmd);
+		}
+
+	}
+}
