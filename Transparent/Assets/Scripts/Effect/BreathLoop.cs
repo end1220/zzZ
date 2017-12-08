b7448fc46dcdf7c203b8733c3f1ac07645d2207b
liucj01 <liucj01@mail.jj.cn>
2017-12-08 12:39:47 +0800

    tools

---
+++ Transparent/Assets/Scripts/Effect/BreathLoop.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+
+public class BreathLoop : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public float minSize = 0.8f;
+	public float maxSize = 1.5f;
+	private float angle = 0;
+
+	private void Update()
+	{
+		angle += speed * Time.deltaTime;
+		angle %= 2 * Mathf.PI;
+		float midSize = (minSize + maxSize) * 0.5f;
+		float factor = (maxSize - minSize) * 0.5f;
+		float scale = Mathf.Cos(angle) * factor + midSize;
+		transform.localScale = Vector3.one * scale;
+	}
+
+}
---
+++ Transparent/Assets/Scripts/Effect/RotateSelf.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RotateSelf : MonoBehaviour
+{
+	public float x = 0f;
+	public float y = 0f;
+	public float z = 0f;
+
+
+	void Update()
+	{
+		if (!Mathf.Approximately(x, 0))
+			transform.Rotate(Time.deltaTime * x, 0, 0, Space.World);
+		if (!Mathf.Approximately(y, 0))
+			transform.Rotate(0, Time.deltaTime * y, 0, Space.World);
+		if (!Mathf.Approximately(z, 0))
+			transform.Rotate(0, 0, Time.deltaTime * z, Space.World);
+	}
+
+}
