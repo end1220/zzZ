b7400edfc04f569f871054fcfdd70bb62088820b
Locke <1534921818@qq.com>
2018-03-12 09:23:23 +0800

    about

--- Float/Float/Pages/Settings/About.xaml.cs
+++ Float/Float/Pages/Settings/About.xaml.cs
@@ -23,6 +23,8 @@ namespace Float.Pages.Settings
 		public About()
 		{
 			InitializeComponent();
+
+			aboutText.Text = "ABOUT是韓國時尚男女裝行動購物品牌 時尚潮流兼具 打造一個全新的自己。\n將生活的感觸化成專業舒適時尚的衣著,分享最新商品、最新消息與即時優惠外,\n也與您分享日常中的各種感觸,一起成長學習。";
 		}
 	}
 }
