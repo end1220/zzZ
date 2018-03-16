b9e026b1af6a29a3ffe371c30014b3ffbe916a47
Locke <1534921818@qq.com>
2018-03-16 09:09:07 +0800

    iii

--- Transparent/Assets/Editor/Workshop/WorkshoplWindow.cs
+++ Transparent/Assets/Editor/Workshop/WorkshoplWindow.cs
@@ -27,63 +27,76 @@ namespace Lite
 
 		void OnGUI()
 		{
-			float spaceSize = 10f;
-			float leftSpace = 10;
-			float titleLen = 70;
-			float textLen = 450;
-			float buttonLen1 = 100;
-			float buttonLen2 = 50;
-			float buttonHeight = 40;
-
-			GUILayout.Label("Upload to Steam Workshop", EditorStyles.helpBox);
-			GUILayout.Space(spaceSize);
-
-			GUILayout.BeginHorizontal();
-			GUILayout.Space(leftSpace);
-			GUILayout.Label("Zip File", EditorStyles.label, GUILayout.Width(titleLen));
-			string savedZipPath = EditorPrefs.GetString("BMW_ZipPath");
-			zipFilePath = string.IsNullOrEmpty(savedZipPath) ? zipFilePath : savedZipPath;
-			zipFilePath = GUILayout.TextField(zipFilePath, GUILayout.Width(textLen));
-			if (GUILayout.Button("Select", GUILayout.Width(buttonLen2)))
+			if (SteamManager.Initialized)
 			{
-				zipFilePath = EditorUtility.OpenFilePanel("Select Zip File", String.Empty, "*zip");
-				if (!string.IsNullOrEmpty(zipFilePath))
-					EditorPrefs.SetString("BMW_ZipPath", zipFilePath);
+				float spaceSize = 10f;
+				float leftSpace = 10;
+				float titleLen = 70;
+				float textLen = 450;
+				float buttonLen1 = 100;
+				float buttonLen2 = 50;
+				float buttonHeight = 40;
+
+				GUILayout.Label("Workshop", EditorStyles.helpBox);
+				GUILayout.Space(spaceSize);
+
+				GUILayout.BeginHorizontal();
+				GUILayout.Space(leftSpace);
+				GUILayout.Label("Zip File", EditorStyles.label, GUILayout.Width(titleLen));
+				string savedZipPath = EditorPrefs.GetString("BMW_ZipPath");
+				zipFilePath = string.IsNullOrEmpty(savedZipPath) ? zipFilePath : savedZipPath;
+				zipFilePath = GUILayout.TextField(zipFilePath, GUILayout.Width(textLen));
+				if (GUILayout.Button("Select", GUILayout.Width(buttonLen2)))
+				{
+					zipFilePath = EditorUtility.OpenFilePanel("Select Zip File", String.Empty, "*zip");
+					if (!string.IsNullOrEmpty(zipFilePath))
+						EditorPrefs.SetString("BMW_ZipPath", zipFilePath);
+				}
+				GUILayout.EndHorizontal();
+				GUILayout.Space(spaceSize);
+
+				GUILayout.BeginHorizontal();
+				GUILayout.Space(spaceSize);
+				GUILayout.Label("Title", EditorStyles.label, GUILayout.Width(titleLen));
+				titleDesc = GUILayout.TextField(titleDesc, GUILayout.Width(textLen));
+				GUILayout.EndHorizontal();
+				GUILayout.Space(leftSpace);
+
+				GUILayout.BeginHorizontal();
+				GUILayout.Space(spaceSize);
+				GUILayout.Label("Preview", EditorStyles.label, GUILayout.Width(titleLen));
+				string savedPreviewPath = EditorPrefs.GetString("BMW_PreviewPath");
+				previewFilePath = string.IsNullOrEmpty(savedPreviewPath) ? previewFilePath : savedPreviewPath;
+				previewFilePath = GUILayout.TextField(previewFilePath, GUILayout.Width(textLen));
+				if (GUILayout.Button("Select", GUILayout.Width(buttonLen2)))
+				{
+					zipFilePath = EditorUtility.OpenFilePanel("Select Preview File", String.Empty, "*.jpg");
+					if (!string.IsNullOrEmpty(zipFilePath))
+						EditorPrefs.SetString("BMW_PreviewPath", zipFilePath);
+				}
+				GUILayout.EndHorizontal();
+				GUILayout.Space(leftSpace);
+
+				GUILayout.BeginHorizontal();
+				GUILayout.Space(leftSpace);
+				if (GUILayout.Button("Upload", GUILayout.Width(buttonLen1), GUILayout.Height(buttonHeight)))
+				{
+
+				}
+
+				GUILayout.EndHorizontal();
+				GUILayout.Space(spaceSize);
 			}
-			GUILayout.EndHorizontal();
-			GUILayout.Space(spaceSize);
-
-			GUILayout.BeginHorizontal();
-			GUILayout.Space(spaceSize);
-			GUILayout.Label("Title", EditorStyles.label, GUILayout.Width(titleLen));
-			titleDesc = GUILayout.TextField(titleDesc, GUILayout.Width(textLen));
-			GUILayout.EndHorizontal();
-			GUILayout.Space(leftSpace);
-
-			GUILayout.BeginHorizontal();
-			GUILayout.Space(spaceSize);
-			GUILayout.Label("Preview", EditorStyles.label, GUILayout.Width(titleLen));
-			string savedPreviewPath = EditorPrefs.GetString("BMW_PreviewPath");
-			previewFilePath = string.IsNullOrEmpty(savedPreviewPath) ? previewFilePath : savedPreviewPath;
-			previewFilePath = GUILayout.TextField(previewFilePath, GUILayout.Width(textLen));
-			if (GUILayout.Button("Select", GUILayout.Width(buttonLen2)))
+			else
 			{
-				zipFilePath = EditorUtility.OpenFilePanel("Select Preview File", String.Empty, "*.jpg");
-				if (!string.IsNullOrEmpty(zipFilePath))
-					EditorPrefs.SetString("BMW_PreviewPath", zipFilePath);
+				GUILayout.Label("SteamAPI failed", EditorStyles.label);
 			}
-			GUILayout.EndHorizontal();
-			GUILayout.Space(leftSpace);
+		}
 
-			GUILayout.BeginHorizontal();
-			GUILayout.Space(leftSpace);
-			if (GUILayout.Button("Upload", GUILayout.Width(buttonLen1), GUILayout.Height(buttonHeight)))
-			{
-				
-			}
-			
-			GUILayout.EndHorizontal();
-			GUILayout.Space(spaceSize);
+		private void Awake()
+		{
+			if (!SteamManager.Initialized)
+				SteamManager.Instance.Init();
 		}
 
 	}
